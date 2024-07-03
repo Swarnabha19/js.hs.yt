edf1c207fcfbc909741f14b0c031d7872be52182
Swarnabha19 <142039518+Swarnabha19@users.noreply.github.com>
2024-07-03 11:59:28 +0000

    conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -49,7 +49,7 @@ null => 0 */
 // let login = 0
 
 // let booleanLoggedin = Boolean(isLoggedIn)
-// //console.log(typeof booleanLoggedin);
+// console.log(typeof booleanLoggedin);
 // console.log(booleanLoggedin);
 
 
@@ -60,7 +60,55 @@ null => 0 */
 // "tojo" => true
 
 
-let digit = 34
-let stringNumber= String(digit)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let digit = 34
+// let stringNumber= String(digit)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//******************operations******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+//console.table([2+2, 2-2, 2*2, 2**3, 2/3, 2%3]);
+
+let str1 ="hello"
+let str2 =" swarnabha"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2); // all conversion is in strings
+// console.log(1 + "2" + 3);
+// console.log(1 + 2 + "2"); // 1+2=3 + string 2
+// console.log( 3 + 4 * 5 % 3);
+
+// console.log(+true); //true is boolean which is converted into number 1
+// console.log(+"") 
+// console.log(+false);
+// console.log(typeof false);
+
+let num1, num2, num3
+
+num1=num2=num3= 2+2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter++);
+console.log(++gameCounter);
+console.log(gameCounter++);
+console.log(gameCounter);
+
+let x = 3;
+console.log(++x);
+console.log(x++);
+// const y = x++;
+// console.log(x);
+// console.log(y);
+
