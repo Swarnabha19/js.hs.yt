c4c9e8e388806905b69cdd8b1226b08ff661d74c
Swarnabha19 <142039518+Swarnabha19@users.noreply.github.com>
2024-07-03 19:23:06 +0000

    datatypememory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -22,7 +22,7 @@ let nam = 'tojo'
 console.log('tojo');
 
 
-//Reference (Non Primitive)
+//Reference (Non Primitive) ====>>> call by reference
 
 //Array , Objects , Functions  // these datatypes are object
 // only function returns functions called function object
@@ -37,6 +37,9 @@ name: "tojo" ,
 age: 22 ,
 } 
 
+console.log(myObj.age)
+console.log(myObj.name)
+
 console.log(myObj);
 console.log(typeof myObj);
  
@@ -50,3 +53,30 @@ const myFunction = function()
 console.log(typeof myFunction);
 console.log( typeof myObj)
 console.log(typeof heros);
+
+
+//*************************************************************** 
+
+// stack(primitive) , heap memory(non Primitive) => call by reference
+
+let myYoutubename = "swarker"
+
+let anothername = myYoutubename
+anothername = "tojosarker"
+
+console.log(anothername);
+console.log(myYoutubename); // stack
+
+let userOne = 
+{
+email: "user@gmail.com" ,
+upiId: "user@oksbi"
+}
+
+let userTwo = userOne
+
+userTwo.email = "tojo@gmail.com"
+
+console.log(userOne.email); // heap memory
+console.log(userTwo.email);
+
